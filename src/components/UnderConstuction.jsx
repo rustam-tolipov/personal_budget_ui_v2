3a83b8d06b628f8cae74d1fd889cd70d61555f2e
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-16 09:21:05 +0500

    Under construction page

---
+++ src/components/UnderConstuction.jsx
@@ -0,0 +1,13 @@
+import underConsturction from '../assets/under.jpg';
+
+import styles from './UnderConsturction.module.css';
+
+const UnderConsturction = () => {
+  return (
+    <div className={styles.container}>
+      <img className={styles.img} src={underConsturction} />;
+    </div>
+  );
+};
+
+export default UnderConsturction;
