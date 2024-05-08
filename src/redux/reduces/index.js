c5ce02892e0118886a281855a187d629e41b1f4b
rustam-tolipov <rustamxontolipov788@gmail.com>
2024-05-08 11:51:16 +0500

    refactor: Remove unused CSS and Redux files

--- src/redux/reduces/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import categoriesReducer from './categoriesReducer';
-
-export default combineReducers({
-  auth: authReducer,
-  categories: categoriesReducer,
-});
