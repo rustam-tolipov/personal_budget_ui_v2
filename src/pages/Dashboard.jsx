eb16a38187d342ee2b57e2d302c4f757dd4dde16
rustam-tolipov <rustamxontolipov788@gmail.com>
2024-05-14 16:34:21 +0500

    Add shadow to dashboard header

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,11 +5,11 @@ import { SearchBar } from "../ui/Searbar";
 const Dashboard = () => {
   return (
     <div className="flex h-full flex-col">
-      <div className="flex h-20 items-center bg-white px-4 shadow-lg">
+      <div className="flex h-20 items-center bg-white px-4 shadow-3xl">
         <SearchBar />
         <BellIcon className="ml-auto h-8 w-8 text-gray-900" />
       </div>
-      <div className="h-full w-full bg-slate-100 p-4">Dashboard</div>
+      <div className="h-full w-full bg-[#F1F1F1] p-4">Dashboard</div>
     </div>
   );
 };
