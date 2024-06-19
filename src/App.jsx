fa199b7f3024cd2ab54ec4c92075643dd9832e62
rustam-tolipov <rustamxontolipov788@gmail.com>
2024-06-19 15:58:40 +0500

    feat: Add new pages for Calendar, Currency, Dashboard, Expenses, Incomes, and Savings

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
 
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import AppLayout from './ui/AppLayout';
+import Loader from './ui/Loader';
+
+import Dashboard from './pages/Dashboard';
+
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Incomes from './pages/Incomes';
+import Expenses from './pages/Expenses';
+import Calendar from './pages/Calendar';
+import Savings from './pages/Savings';
+import Currency from './pages/Currency';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="sign-up" element={<Signup />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='incomes' element={<Incomes />} />
+            <Route path='expenses' element={<Expenses />} />
+            <Route path='calendar' element={<Calendar />} />
+            <Route path='savings' element={<Savings />} />
+            <Route path='currency' element={<Currency />} />
+          </Route>
+          <Route path='login' element={<Login />} />
+          <Route path='sign-up' element={<Signup />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
